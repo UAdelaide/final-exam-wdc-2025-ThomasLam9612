76ef5ad544a4e30bb912cf91369036e5575caa40
ThomasLam9612 <ThomasLam9612@gmail.com>
2025-06-20 04:51:56 +0000

    [automatic commit] 2025-06-20 04:51:56 UTC 58.174.80.93

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -87,7 +87,7 @@ router.get('/getPets', async (req, res) => {
   try {
     const [rows] = await db.query(
       `SELECT dog_id, name FROM Dogs WHERE owner_id = ?`,
-      [req.session.user.user_id]
+      [req.session.user.username]
     );
     res.json(rows);
     console.log(rows);
