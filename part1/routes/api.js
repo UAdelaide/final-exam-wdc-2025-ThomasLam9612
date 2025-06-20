724b2e3d6d53ddc1901fc38454c18f796432ed1b
ThomasLam9612 <ThomasLam9612@gmail.com>
2025-06-20 01:19:51 +0000

    [automatic commit] 2025-06-20 01:19:51 UTC 58.174.80.93

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -17,7 +17,7 @@ const pool = db.createPool({
  */
 const SELECT_Dog_Info = "SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username  FROM Dogs Left Join Users ON owner_id = Users.user_id";
 router.get('/dogs', function(req, res, next) {
-    const [rows] = pool.query(SELECT_Dog_Info);
+    const [rows] = await pool.query(SELECT_Dog_Info);
     return res.send(rows);
 });
 
