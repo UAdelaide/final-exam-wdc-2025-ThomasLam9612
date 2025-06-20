0c9f5ed7c71ec0c613cd2173aef6ab89fd2083d1
ThomasLam9612 <ThomasLam9612@gmail.com>
2025-06-20 01:51:54 +0000

    [automatic commit] 2025-06-20 01:51:54 UTC 58.174.80.93

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -28,7 +28,7 @@ const summary = `SELECT
     SELECT COUNT(*)
     FROM WalkRequests wr2
     JOIN WalkApplications wa2 ON wr2.request_id = wa2.request_id
-    WHERE wa2.walker_id = u.user_id AND wr2.status = 'completed'
+    --WHERE wa2.walker_id = u.user_id AND wr2.status = 'completed'
   ) AS completed_walks
 FROM Users u
 LEFT JOIN WalkRatings r ON r.walker_id = u.user_id
