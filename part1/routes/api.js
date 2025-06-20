6e2e07db0fd708a5b47cfa67765bc7d3a180e178
ThomasLam9612 <ThomasLam9612@gmail.com>
2025-06-20 01:12:24 +0000

    [automatic commit] 2025-06-20 01:12:24 UTC 58.174.80.93

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,5 +1,5 @@
 const db = require("mysql2/promise");
-db.createPool({
+const pool = db.createPool({
     host:localhost,
     user:root,
     password:root,
@@ -15,5 +15,5 @@ db.createPool({
  */
 const SELECT_Dog_Info = "SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username  FROM Dogs Left Join Users ON owner_id = Users.user_id";
 router.get('/', function(req, res, next) {
-    db.
+    pool.query(Se)
 });
