07176f31eeed171ea3b5a3a81536de40cdbf3e9d
ThomasLam9612 <ThomasLam9612@gmail.com>
2025-06-20 04:46:15 +0000

    [automatic commit] 2025-06-20 04:46:14 UTC 58.174.80.93

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -89,7 +89,6 @@ router.get('/getPets', async (req, res) => {
       `SELECT dog_id, name FROM Dogs WHERE owner_id = ?`,
       [req.session.user.user_id]
     );
-
     res.json(rows);
   } catch (err) {
     res.status(500).json({ error: 'Failed to load dogs' });
