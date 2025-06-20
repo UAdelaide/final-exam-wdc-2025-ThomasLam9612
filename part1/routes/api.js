17e9849951efdd6b3d1e6d2d1bd4caf39588e74d
ThomasLam9612 <ThomasLam9612@gmail.com>
2025-06-20 01:05:02 +0000

    [automatic commit] 2025-06-20 01:05:02 UTC 58.174.80.93

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,7 +1,9 @@
 const db = require("mysql2/promise");
 db.createPool({
     host:localhost,
-    
+    user:root,
+    password:root,
+    database:
 
 })
 router.get('/', function(req, res, next) {
