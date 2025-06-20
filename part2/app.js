da3fa981f619b6a1f676ef7598223351be051151
ThomasLam9612 <ThomasLam9612@gmail.com>
2025-06-20 04:02:45 +0000

    [automatic commit] 2025-06-20 04:02:45 UTC 58.174.80.93

--- part2/app.js
+++ part2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const session = require("express-session");
-require("../")
+require("moddels/db.js")
 const MySQLStore  = require("express-mysql-session")(session);
 
 const app = express();
